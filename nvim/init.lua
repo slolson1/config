3d7e3f568ccac2e0afff8528564e416b12cd6341
Shane Olson <shaneolson@machindustries.com>
2025-12-01 11:24:17 -0800

    added nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,23 @@
+-- Global defaults for SystemVerilog
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+
+-- Override for Makefiles
+vim.api.nvim_create_augroup("makefile_indent", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "makefile_indent",
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 8
+    vim.opt_local.shiftwidth = 8
+    vim.opt_local.softtabstop = 0
+  end,
+})
+
+-- Extend copy/paste to system
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+
