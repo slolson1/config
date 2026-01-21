1679f72cf5f8040b80245c65a9176034d2d0c99c
Shane Olson <shaneolson@machindustries.com>
2026-01-20 16:55:13 -0800

    enabled inlay diagnostic messages

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,6 +4,10 @@ return {
     config = function()
       vim.lsp.enable('verible')
       vim.lsp.enable('svlangserver')
+
+      vim.diagnostic.config({
+        virtual_text = true,
+      })
     end,
   },
 }
